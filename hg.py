159da0e9a98c88d9b24aca4a4e70bcbecb62543a
SimphiweGithub <simphiwe0105@gmail.com>
2025-02-20 20:09:23 +0200

    update

--- hg.py
+++ hg.py
@@ -1,19 +1,55 @@
 import datetime
 import json
+import random
 
 
 
 tasks = {}
+hold = []
+
 def deleteTask():
-    delTak = ""
+    
+    with open('l.json', 'r') as openfile:
+        data = json.load(openfile)  
+
+    value_to_remove = 1  
+
+    
+    updated_data = []
+    removed_count = 0
+
+    for item in data:
+        if value_to_remove in item.values():
+            removed_count += 1  # Count removed items
+        else:
+            updated_data.append(item)  # Keep the item if it doesn't match
+
+    if removed_count > 0:
+        print(f"Removed {removed_count} item(s).")
+    else:
+        print(f"No item found with value '{value_to_remove}'.")
+
+    # Save the updated JSON data back to the file
+    with open('l.json', 'w') as writefile:
+        json.dump(updated_data, writefile, indent=2)
 
 def updateTask():  
+    with open('l.json', 'r') as openfile:
+        data = json.load(openfile)
+        id_to_search_for = input("Enter the task number you want to update:")
+
+
+
+    for item in data:
+        if id_to_search_for in item.values():
+          print() 
+
     updatedAt = datetime.datetime.now()
     tasks["updatedAt"] = updatedAt
 
 
-def createTask():
-    id= input("test id:")#gonna have to
+def addTask():
+    id= input("What ID do you want to give:")
     description = input("Description of task:")
     status = input("test status:")
     createdAt = str(datetime.datetime.now())
@@ -25,9 +61,17 @@ def createTask():
     tasks["status"] = status
     tasks["createdAt"] = createdAt
     tasks["updatedAt"]  = ""
-    #data = tasks.json() 
+    
+    hold.append(tasks) 
     with open("l.json", "w") as file:
-        json.dump(tasks,file)
-    print(tasks)
+        json.dump(hold,file,indent=2)
+    
+def listAllTasks():
+    with open('l.json', 'r') as openfile:
+        data = json.load(openfile)
+        print(data)
+        lm = str(data)
+    lm = str(data).replace("[", "").replace("]", "").replace("'","").replace(",","\n").replace("{", "").replace("}", "")
+    print(lm)
 
-createTask()  
+listAllTasks()
